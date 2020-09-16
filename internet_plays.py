d304f03e1a0d55ffe297930954a5790962c167a4
Likai Kuroi <likai31197@gmail.com>
2020-09-16 15:54:53 -0400

    re-added dual wield handling

--- internet_plays.py
+++ internet_plays.py
@@ -407,7 +407,7 @@ def handle_action_command(button, j, **kwargs):
         elif button == "shootlong":
             j.set_button(acceptable_commands[button], 1) # press the right button
             return shoot_long_duration
-        elif button == "use":
+        elif button == "use" or button == "dualwieldleft" or button == "dualwieldright" or button == "reloadleft":
             j.set_button(acceptable_commands[button], 1) # press the right button
             return press_duration
         else:
